4f478e18119d541876eb78c46ffd6b7af71f6695
Phornchita Suttiborriban <phorhita.ed@gmail.com>
2024-06-03 14:19:34 +0200

    Add vai uploads
    from a game in 3 hours folder

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace GameIn3Hours
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MoveBGTimer = new System.Windows.Forms.Timer(this.components);
+            this.Ninja = new System.Windows.Forms.PictureBox();
+            this.UpTimer = new System.Windows.Forms.Timer(this.components);
+            this.DownTimer = new System.Windows.Forms.Timer(this.components);
+            this.LeftTimer = new System.Windows.Forms.Timer(this.components);
+            this.RightTimer = new System.Windows.Forms.Timer(this.components);
+            this.WepaonsMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.FruitsTimer = new System.Windows.Forms.Timer(this.components);
+            this.FruitsShootTimer = new System.Windows.Forms.Timer(this.components);
+            this.Exit = new System.Windows.Forms.Button();
+            this.Replay = new System.Windows.Forms.Button();
+            this.GameOverLabel = new System.Windows.Forms.Label();
+            this.ShowLevel = new System.Windows.Forms.Label();
+            this.ShowScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Ninja)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MoveBGTimer
+            // 
+            this.MoveBGTimer.Enabled = true;
+            this.MoveBGTimer.Interval = 10;
+            this.MoveBGTimer.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Ninja
+            // 
+            this.Ninja.BackColor = System.Drawing.Color.Transparent;
+            this.Ninja.Image = ((System.Drawing.Image)(resources.GetObject("Ninja.Image")));
+            this.Ninja.Location = new System.Drawing.Point(261, 400);
+            this.Ninja.Name = "Ninja";
+            this.Ninja.Size = new System.Drawing.Size(61, 54);
+            this.Ninja.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ninja.TabIndex = 1;
+            this.Ninja.TabStop = false;
+            // 
+            // UpTimer
+            // 
+            this.UpTimer.Interval = 5;
+            this.UpTimer.Tick += new System.EventHandler(this.Uptimer_Tick);
+            // 
+            // DownTimer
+            // 
+            this.DownTimer.Interval = 5;
+            this.DownTimer.Tick += new System.EventHandler(this.DownTimer_Tick);
+            // 
+            // LeftTimer
+            // 
+            this.LeftTimer.Interval = 5;
+            this.LeftTimer.Tick += new System.EventHandler(this.LeftTimer_Tick);
+            // 
+            // RightTimer
+            // 
+            this.RightTimer.Interval = 5;
+            this.RightTimer.Tick += new System.EventHandler(this.RightTimer_Tick);
+            // 
+            // WepaonsMoveTimer
+            // 
+            this.WepaonsMoveTimer.Enabled = true;
+            this.WepaonsMoveTimer.Interval = 20;
+            this.WepaonsMoveTimer.Tick += new System.EventHandler(this.WepaonsMoveTimer_Tick);
+            // 
+            // FruitsTimer
+            // 
+            this.FruitsTimer.Enabled = true;
+            this.FruitsTimer.Tick += new System.EventHandler(this.FruitsTimer_Tick);
+            // 
+            // FruitsShootTimer
+            // 
+            this.FruitsShootTimer.Enabled = true;
+            this.FruitsShootTimer.Interval = 20;
+            this.FruitsShootTimer.Tick += new System.EventHandler(this.FruitsShootTimer_Tick);
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Exit.Font = new System.Drawing.Font("Goudy Stout", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.Location = new System.Drawing.Point(212, 313);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(159, 59);
+            this.Exit.TabIndex = 3;
+            this.Exit.Text = "Exit?";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Visible = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Replay
+            // 
+            this.Replay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Replay.Font = new System.Drawing.Font("Goudy Stout", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Replay.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Replay.Location = new System.Drawing.Point(175, 219);
+            this.Replay.Name = "Replay";
+            this.Replay.Size = new System.Drawing.Size(227, 70);
+            this.Replay.TabIndex = 5;
+            this.Replay.Text = "Replay?";
+            this.Replay.UseVisualStyleBackColor = false;
+            this.Replay.Visible = false;
+            this.Replay.Click += new System.EventHandler(this.Replay_Click);
+            // 
+            // GameOverLabel
+            // 
+            this.GameOverLabel.BackColor = System.Drawing.Color.Transparent;
+            this.GameOverLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.GameOverLabel.Enabled = false;
+            this.GameOverLabel.Font = new System.Drawing.Font("Goudy Stout", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameOverLabel.ForeColor = System.Drawing.Color.Red;
+            this.GameOverLabel.Location = new System.Drawing.Point(92, 96);
+            this.GameOverLabel.Name = "GameOverLabel";
+            this.GameOverLabel.Size = new System.Drawing.Size(401, 80);
+            this.GameOverLabel.TabIndex = 6;
+            this.GameOverLabel.Text = "Game Over";
+            this.GameOverLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.GameOverLabel.Visible = false;
+            // 
+            // ShowLevel
+            // 
+            this.ShowLevel.AutoSize = true;
+            this.ShowLevel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ShowLevel.Font = new System.Drawing.Font("Goudy Stout", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShowLevel.Location = new System.Drawing.Point(464, 9);
+            this.ShowLevel.Name = "ShowLevel";
+            this.ShowLevel.Size = new System.Drawing.Size(112, 27);
+            this.ShowLevel.TabIndex = 7;
+            this.ShowLevel.Text = "Level ";
+            this.ShowLevel.UseCompatibleTextRendering = true;
+            // 
+            // ShowScore
+            // 
+            this.ShowScore.AutoSize = true;
+            this.ShowScore.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ShowScore.Font = new System.Drawing.Font("Goudy Stout", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShowScore.Location = new System.Drawing.Point(12, 9);
+            this.ShowScore.Name = "ShowScore";
+            this.ShowScore.Size = new System.Drawing.Size(102, 22);
+            this.ShowScore.TabIndex = 8;
+            this.ShowScore.Text = "Score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(588, 454);
+            this.Controls.Add(this.ShowScore);
+            this.Controls.Add(this.ShowLevel);
+            this.Controls.Add(this.GameOverLabel);
+            this.Controls.Add(this.Replay);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Ninja);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(610, 510);
+            this.MinimumSize = new System.Drawing.Size(610, 510);
+            this.Name = "Form1";
+            this.Text = "FruitsNinja";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Ninja)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MoveBGTimer;
+        private System.Windows.Forms.PictureBox Ninja;
+        private System.Windows.Forms.Timer UpTimer;
+        private System.Windows.Forms.Timer DownTimer;
+        private System.Windows.Forms.Timer LeftTimer;
+        private System.Windows.Forms.Timer RightTimer;
+        private System.Windows.Forms.Timer WepaonsMoveTimer;
+        private System.Windows.Forms.Timer FruitsTimer;
+        private System.Windows.Forms.Timer FruitsShootTimer;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Replay;
+        private System.Windows.Forms.Label GameOverLabel;
+        private System.Windows.Forms.Label ShowLevel;
+        private System.Windows.Forms.Label ShowScore;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,457 @@
+﻿using DocumentFormat.OpenXml.Math;
+using DocumentFormat.OpenXml;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection.Emit;
+using System.Security.AccessControl;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace GameIn3Hours
+{
+    public partial class Form1 : Form
+    {
+       
+        WindowsMediaPlayer backgroundSond;
+        WindowsMediaPlayer weapondsSond;
+        WindowsMediaPlayer fruitsExplodeSound;
+
+        PictureBox[] stars;
+        int backgroundSpeed;
+        int ninjaSpeed;
+        Random rnd;
+        PictureBox imageControl;
+
+        PictureBox[] weapons;
+        int weaponsSpeed;
+
+        PictureBox[] fruits;
+        int fruitsSpeed;
+
+        PictureBox fruitsShooter;
+        int fruitsShooterSpeed;
+
+        int score;
+        int level;
+        int dificulty;
+        bool pause;
+        bool gameOver;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load1;
+        }
+
+        private void Form1_Load1(object sender, EventArgs e)
+        {
+                pause = false;
+                gameOver = false;
+                score = 0;
+                level = 1;
+                dificulty = 9;
+
+                //fruits as enemies
+
+                Image apple = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\apples.png");
+                Image banana = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\banana.png");
+                Image berry = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\berry.png");
+                Image watermelon = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\watermelon.png");
+                Image orange = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\orange.png");
+                Image pear = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\pears.png");
+                Image strawberry = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\strawsberry.png");
+                Image peach = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\peach.png");
+                Image pineapple = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\pineapple.png");
+                Image kiwi = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\kiwi.png");
+                Image bossChilly = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\chilly.png");
+                Image bossGarlic = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\garlic.png");
+
+                fruits = new PictureBox[12];
+                fruitsSpeed = 5;
+
+                for (int i = 0; i < fruits.Length; i++)
+                {
+                    fruits[i] = new PictureBox();
+                    fruits[i].Size = new Size(35, 35);
+                    fruits[i].SizeMode = PictureBoxSizeMode.Zoom;
+                    fruits[i].BorderStyle = BorderStyle.None;
+                    fruits[i].BackColor = Color.Transparent;
+                    fruits[i].Visible = false;
+                    this.Controls.Add(fruits[i]);
+                    fruits[i].Location = new Point((i + 1) * 30, -30);
+                }
+
+                fruits[0].Image = apple;
+                fruits[1].Image = banana;
+                fruits[2].Image = berry;
+                fruits[3].Image = watermelon;
+                fruits[4].Image = orange;
+                fruits[5].Image = pear;
+                fruits[6].Image = strawberry;
+                fruits[7].Image = peach;
+                fruits[8].Image = pineapple;
+                fruits[9].Image = kiwi;
+                fruits[10].Image = bossChilly;
+                fruits[11].Image = bossGarlic;
+
+                //sound and SFX
+                backgroundSond = new WindowsMediaPlayer();
+                weapondsSond = new WindowsMediaPlayer();
+                fruitsExplodeSound = new WindowsMediaPlayer();
+
+                backgroundSond.URL = (@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\BGmusic.mp3");
+                weapondsSond.URL = (@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\WeaponsMusic.mp3");
+                fruitsExplodeSound.URL = (@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\FruitsExplode.mp3");
+
+
+                backgroundSond.settings.setMode("loop", true);
+                backgroundSond.settings.volume = 10;
+                weapondsSond.settings.volume = 4;
+                fruitsExplodeSound.settings.volume = 6;
+
+                backgroundSond.controls.play();
+
+                //weapon of the ninja (I have a lot of typo on the weapons's function so beware!)
+                weaponsSpeed = 30;
+                weapons = new PictureBox[5];
+
+                Image shuriken = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\shuriken.png");
+
+                for (int i = 0; i < weapons.Length; i++)
+                {
+                    weapons[i] = new PictureBox();
+                    weapons[i].Image = shuriken;
+                    weapons[i].Size = new Size(20, 20);
+                    weapons[i].SizeMode = PictureBoxSizeMode.Zoom;
+                    weapons[i].BorderStyle = BorderStyle.None;
+                    weapons[i].BackColor = Color.Transparent;
+                    this.Controls.Add(weapons[i]);
+                }
+
+                //BG and stars
+                backgroundSpeed = 4;
+                ninjaSpeed = 8;
+                stars = new PictureBox[15];
+                rnd = new Random();
+
+                for (int i = 0; i < stars.Length; i++)
+                {
+                    stars[i] = new PictureBox();
+                    stars[i].BorderStyle = BorderStyle.None;
+                    stars[i].Location = new Point(rnd.Next(20, 588), rnd.Next(-10, 400));
+
+                    if (i % 3 == 1)
+                    {
+                        stars[i].Size = new Size(1, 1);
+                        stars[i].BackColor = Color.White;
+
+                    }
+                    else
+                    {
+                        stars[i].Size = new Size(2, 2);
+                        stars[i].BackColor = Color.Gray;
+                    }
+                    this.Controls.Add(stars[i]);
+                }
+                //fruits's weapons
+                fruitsShooterSpeed = 5;
+                fruitsShooter = new PictureBox();
+
+                Image fruitShooter = Image.FromFile(@"C:\Users\pornc\Desktop\C\c#\GameIn3Hours\assets\FruitAmmo.png");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    fruitsShooter = new PictureBox();
+                    fruitsShooter.Image = fruitShooter;
+                    fruitsShooter.Size = new Size(12, 12);
+                    fruitsShooter.SizeMode = PictureBoxSizeMode.Zoom;
+                    fruitsShooter.BorderStyle = BorderStyle.None;
+                    fruitsShooter.BackColor = Color.Transparent;
+                    int x = rnd.Next(0, 10);
+                    fruitsShooter.Location = new Point(fruits[x].Location.X + 12, fruits[x].Location.Y - 10);
+                    this.Controls.Add(fruitsShooter);
+                }
+
+            }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+            {
+                for (int i = 0; i < stars.Length / 2; i++)
+                {
+                    stars[i].Top += backgroundSpeed;
+                    if (stars[i].Top >= this.Height)
+                    {
+                        stars[i].Top = stars[i].Height;
+                    }
+                    for (i = stars.Length / 2; i < stars.Length; i++)
+                    {
+                        stars[i].Top += backgroundSpeed;
+                        if (stars[i].Top >= this.Height)
+                        {
+                            stars[i].Top = stars[i].Height;
+                        }
+                    }
+                }
+            }
+
+            private void Uptimer_Tick(object sender, EventArgs e)
+            {
+                if (Ninja.Top > 10)
+                {
+                    Ninja.Top -= ninjaSpeed;
+                }
+            }
+
+            private void DownTimer_Tick(object sender, EventArgs e)
+            {
+                if (Ninja.Top < 270)
+                {
+                    Ninja.Top += ninjaSpeed;
+                }
+            }
+
+            private void LeftTimer_Tick(object sender, EventArgs e)
+            {
+                if (Ninja.Left > 10)
+                {
+                    Ninja.Left -= ninjaSpeed;
+                }
+            }
+
+            private void RightTimer_Tick(object sender, EventArgs e)
+            {
+                if (Ninja.Right < 400)
+                {
+                    Ninja.Left += ninjaSpeed;
+                }
+
+            }
+
+            private void Form1_KeyDown(object sender, KeyEventArgs e)
+            {
+                if (!pause)
+                {
+                    if (e.KeyCode == Keys.Up)
+                    {
+                        UpTimer.Start();
+                    }
+                    if (e.KeyCode == Keys.Down)
+                    {
+                        DownTimer.Start();
+                    }
+                    if (e.KeyCode == Keys.Left)
+                    {
+                        LeftTimer.Start();
+                    }
+                    if (e.KeyCode == Keys.Right)
+                    {
+                        RightTimer.Start();
+                    }
+                }
+            }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            UpTimer.Stop();
+            DownTimer.Stop();
+            LeftTimer.Stop();
+            RightTimer.Stop();
+
+            if (e.KeyCode == Keys.Space)
+            {
+                if (!gameOver)
+                {
+                    if (pause)
+                    {
+                        StartTimer();
+                        GameOverLabel.Visible = false;
+                        backgroundSond.controls.play();
+                        pause = false;
+                    }
+                    else if (e.KeyCode == Keys.Space)
+                    {
+                        GameOverLabel.Visible = true;
+                        GameOverLabel.Location = new Point(70, 90);
+                        GameOverLabel.Text = "PAUSED";
+                        backgroundSond.controls.pause();
+                        StopTimer();
+                        pause = true;
+                    }
+
+                }
+            }
+        }
+
+            private void WepaonsMoveTimer_Tick(object sender, EventArgs e) //spelled wrong I know, I'm sorry
+            {
+                weapondsSond.controls.play();
+
+                for (int i = 0; i < weapons.Length; i++)
+                {
+                    if (weapons[i].Top > 0)
+                    {
+                        weapons[i].Visible = true;
+                        weapons[i].Top -= weaponsSpeed;
+
+                        NinjaDamage();
+                    }
+                    else
+                    {
+                        weapons[i].Visible = false;
+                        weapons[i].Location = new Point(Ninja.Location.X + 20, Ninja.Location.Y + 30);
+
+                    }
+                }
+
+            }
+
+            private void FruitsTimer_Tick(object sender, EventArgs e)
+            {
+                FruitsMove(fruits, fruitsSpeed);
+            }
+            private void FruitsMove(PictureBox[] array, int speed)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i].Visible = true;
+                    array[i].Top += speed;
+
+                    if (array[i].Top > this.Height)
+                    {
+                        array[i].Location = new Point((i + 1) * 50, -200);
+                    }
+                }
+            }
+
+            private void NinjaDamage()
+            {
+                for (int i = 0; i < fruits.Length; i++)
+                {
+                    if (weapons[0].Bounds.IntersectsWith(fruits[i].Bounds)
+                     | weapons[1].Bounds.IntersectsWith(fruits[i].Bounds) || weapons[2].Bounds.IntersectsWith(fruits[i].Bounds))
+                    {
+                        fruitsExplodeSound.controls.play();
+
+                        score += 1;
+                        ShowScore.Text = (score < 10) ? "0" + score.ToString() : score.ToString();
+
+
+                        if (score % 30 == 0)
+                        {
+                            level += 1;
+                            ShowLevel.Text = (level < 10) ? "0" + level.ToString() : level.ToString();
+
+                            if (fruitsSpeed <= 10 && fruitsShooterSpeed <= 10 && dificulty >= 0)
+                            {
+                                dificulty--;
+                                fruitsSpeed++;
+                                fruitsShooterSpeed++;
+                            }
+                            if (level == 20)
+                            {
+                                GameOver("You completed all the level! Well Done!");
+                            }
+
+                        }
+
+                        fruits[i].Location = new Point((i + 1) * 50, -100);
+                    }
+                    if (Ninja.Bounds.IntersectsWith(fruits[i].Bounds))
+                    {
+                        fruitsExplodeSound.settings.volume = 6;
+                        fruitsExplodeSound.controls.play();
+                        Ninja.Visible = false;
+                        weapondsSond.controls.stop();
+                        GameOver("You Died");
+
+                    }
+                    if (Ninja.Bounds.IntersectsWith(fruitsShooter.Bounds))
+                    {
+                        FruitsDamage();
+                    }
+                }
+            }
+
+            private void GameOver(string str)
+            {
+
+                GameOverLabel.Text = str;
+                GameOverLabel.Location = new Point(70, 90);
+                GameOverLabel.Visible = true;
+                Replay.Visible = true;
+                Exit.Visible = true;
+
+                backgroundSond.controls.stop();
+                StopTimer();
+
+            }
+            private void StopTimer()
+            {
+                MoveBGTimer.Stop();
+                FruitsTimer.Stop();
+                WepaonsMoveTimer.Stop();
+                FruitsShootTimer.Stop();
+            }
+            private void StartTimer()
+            {
+                MoveBGTimer.Start();
+                FruitsTimer.Start();
+                WepaonsMoveTimer.Start();
+                FruitsShootTimer.Start();
+            }
+
+            private void FruitsShootTimer_Tick(object sender, EventArgs e)
+            {
+                for (int i = 0; i < 10 - dificulty; i++)
+                {
+                    if (fruitsShooter.Top < this.Height)
+                    {
+                        fruitsShooter.Visible = true;
+                        fruitsShooter.Top += fruitsShooterSpeed;
+                    }
+                    else
+                    {
+                        fruitsShooter.Visible = false;
+                        int x = rnd.Next(0, 10);
+                        fruitsShooter.Location = new Point(fruits[x].Location.X + 20, fruits[x].Location.Y + 20);
+                    }
+                }
+            }
+
+            private void FruitsDamage()
+            {
+                bool died = false;
+                int i = 10;
+                if (Ninja.Visible)
+                {
+                    if (Ninja.Bounds.IntersectsWith(fruitsShooter.Bounds))
+                    {
+                        fruitsExplodeSound.settings.volume = 6;
+                        fruitsExplodeSound.controls.play();
+                        Ninja.Visible = false;
+                        fruitsShooter.Visible = false;
+                        weapondsSond.controls.stop();
+                        GameOver("You Died");
+                        died = true;
+                    }
+
+                }
+
+            }
+
+            private void Replay_Click(object sender, EventArgs e)
+            {
+                this.Controls.Clear();
+                InitializeComponent();
+                Form1_Load1(e, e);
+            }
+
+            private void Exit_Click(object sender, EventArgs e)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+
+
